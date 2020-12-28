5ff96a352d8024875f5968bc40a4ee723d99809e
rudysaniez <r.saniez@ubik-ingenierie.com>
2020-12-28 11:03:56 +0100

    Api implementation and deployment with the docker-maven-plugin.

--- src/main/java/com/me/r2dbc/h2/review/repository/ReviewRepository.java
+++ src/main/java/com/me/r2dbc/h2/review/repository/ReviewRepository.java
@@ -1,28 +1,49 @@
 package com.me.r2dbc.h2.review.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.me.r2dbc.h2.review.bo.Review;
+import com.me.r2dbc.h2.review.bo.ReviewEntity;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Transactional
-public interface ReviewRepository extends ReactiveCrudRepository<Review, Integer> {
+public interface ReviewRepository extends ReactiveCrudRepository<ReviewEntity, Integer> {
 
 	/**
 	 * @param reviewID
-	 * @return mono of {@link Review}
+	 * @return mono of {@link ReviewEntity}
 	 */
 	@Transactional(readOnly = true)
-	public Mono<Review> findByReviewID(Integer reviewID);
+	public Mono<ReviewEntity> findByReviewID(Integer reviewID);
 	
 	/**
 	 * @param productID
 	 * @param page
-	 * @return flux of {@link Review}
+	 * @return flux of {@link ReviewEntity}
 	 */
 	@Transactional(readOnly = true)
-	public Flux<Review> findByProductID(Integer productID);
+	public Flux<ReviewEntity> findByProductID(Integer productID, Pageable page);
+	
+	/**
+	 * @param productID
+	 * @return number of {@link Review} by productID
+	 */
+	@Transactional(readOnly = true)
+	public Mono<Long> countByProductID(Integer productID);
+	
+	/**
+	 * @param page
+	 * @return flux of {@link ReviewEntity}
+	 */
+	@Transactional(readOnly = true)
+	public Flux<ReviewEntity> findByProductIDNotNull(Pageable page);
+
+	/**
+	 * @param productID
+	 * @return mono of {@link Void}
+	 */
+	public Mono<Void> deleteByProductID(Integer productID);
 }
